12368b9917520c19b93665a726899a19075ba530
Edistra <matthieu.desprez@gmail.com>
2015-10-08 19:04:02 +0200

    Update server.js

--- socket/server.js
+++ socket/server.js
@@ -28,5 +28,11 @@ io.sockets.on('connection', function (socket) {
 });
 
 //var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+//var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+//server.listen(port);
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-server.listen(port);
+server.listen( port, ipaddress, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
