1b5bd19ec552c411dd74706d1111b304702133f0
Edistra <Matthieu@MacBook-Pro-de-DESPREZ.local>
2015-10-08 18:44:49 +0200

    first commit

---
+++ socket/app.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var fs = require('fs');
+
+// Chargement du fichier index.html affiché au client
+var server = http.createServer(function(req, res) {
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+
+// Quand on client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket) {
+	socket.login = '';
+	socket.emit('new_msg', {login : socket.login, msg : 'Vous êtes bien connecté !', show_login : false});
+
+	socket.on('login', function (login) {
+		socket.login = login;
+		socket.broadcast.emit('new_msg', {login : socket.login, msg : socket.login+' vient de se connecter !', show_login : false});
+	});	
+	socket.on('new_msg', function (msg) {
+		console.log(socket.login+' : '+msg);
+		socket.broadcast.emit('new_msg', {login : socket.login, msg : msg, show_login : true});
+	});	
+});
+
+
+server.listen(8080);
