362ef7cace0c5999c80317c10406686d80721102
Edistra <matthieu.desprez@gmail.com>
2015-10-08 23:41:13 +0200

    Update server.js

--- socket/server.js
+++ socket/server.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 // Chargement du fichier index.html affiché au client
 var server = http.createServer(function(req, res) {
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
+    fs.readFile('./app.html', 'utf-8', function(error, content) {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(content);
     });
